a4bfa3e5d202e7804cc68d2d5ab74c59ca548f9d
jacopencb <jac.dcs001@gmail.com>
2014-02-25 19:03:04 +0900

    update js

--- js/dailybread.js
+++ js/dailybread.js
@@ -194,8 +194,8 @@ OpenSpending.DailyBread = function (elem) {
       var iconUrl, paper;
       iconUrl = $(e).data('svg-url');
       paper = Raphael(e, iconRad+iconRad,iconRad+iconRad+5);
-      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#FBDA03', stroke: 'none' });
-      paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: 'none', stroke: '#CCC', opacity: .8, 'stroke-dasharray': '- ' });
+      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#CC0000', stroke: 'none' });		// カテゴリ・サブカテゴリアイコン背景色
+      paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: '#FBDA03', stroke: '#CCC', opacity: .8, 'stroke-dasharray': '- ' });
       $.get(iconUrl, function(svg) {
         if (typeof(svg) == "string") {
           svg = $(svg);
