9cdb03f6b989c90e9977728a0904b7bc999a60c1
jacopencb <jac.dcs001@gmail.com>
2014-02-25 15:21:58 +0900

    update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,14 +18,29 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* kuroishi cofog　color=使途一日あたりのアイコン色　bcolor=使途別予算額のアイコン色 */
+ /*
+1：議会費
+2：総務費
+3：民生費
+4：衛生費
+5：労働費
+6：農林水産業費
+7：商工費
+8：土木費
+9：消防費
+10：教育費
+11：災害復旧費
+12：公債費
+13：予備費
+  */
   '1'	: { icon: 'icons/legislative.svg'					, color: '#FF6666', bcolor: '#FF6666' },
   '1.1'	: { icon: 'icons/legislative.svg'					, color: '#FF6666', bcolor: '#FF6666' },
   '2'	: { icon: 'icons/admin.svg'							, color: '#8A0808', bcolor: '#0AB971' },
   '2.1'	: { icon: 'icons/admin.svg'							, color: '#8A0808', bcolor: '#0AB971' },
   '3'	: { icon: 'icons/helping-others.svg'				, color: '#8A0808', bcolor: '#4E6D00' },
   '3.1'	: { icon: 'icons/helping-others.svg'				, color: '#8A0808', bcolor: '#4E6D00' },
-  '4'	: { icon: 'icons/health.svg'						, color: '#8A0808', bcolor: '#D33673' },
-  '4.1'	: { icon: 'icons/health.svg'						, color: '#8A0808', bcolor: '#D33673' },
+  '4'	: { icon: 'icons/health.svg'						, color: '#8A0808', bcolor: '#339900' },
+  '4.1'	: { icon: 'icons/health.svg'						, color: '#8A0808', bcolor: '#339900' },
   '5'	: { icon: 'icons/misc-services.svg'					, color: '#8A0808', bcolor: '#2A3A03' },
   '5.1'	: { icon: 'icons/misc-services.svg'					, color: '#8A0808', bcolor: '#2A3A03' },
   '6'	: { icon: 'icons/forest.svg'						, color: '#8A0808', bcolor: '#EC2406' },
